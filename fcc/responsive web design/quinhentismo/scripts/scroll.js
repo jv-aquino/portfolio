61b8b70306f3a00e68366659fb66dd9bc2e88ed7
jv-aquino <jvaquinocontact@gmail.com>
2022-01-10 20:39:23 -0300

    Adicionar projetos do curso Responsive Web Design e projetos relacionados

---
+++ fcc/responsive web design/quinhentismo/scripts/scroll.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 700, function(){window.location.hash = hash;});}});});
